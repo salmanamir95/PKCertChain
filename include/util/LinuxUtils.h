21ed4b4d1907c1d7cdbba1a3bdc39b3fa532e7e4
Muhammad Salman Amir <mmsa155j3@gmail.com>
2026-03-24 05:13:08 +0500

    Save Keys

--- include/util/LinuxUtils.h
+++ include/util/LinuxUtils.h
@@ -9,8 +9,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #include "datatype/OpStatus.h"
+#include "datatype/uint256_t.h"
 
 #ifndef UTIL_INLINE
 #define UTIL_INLINE static inline __attribute__((always_inline))
@@ -18,6 +20,10 @@
 
 #define PKCERTCHAIN_WALLET_SUBDIR ".pkcertchain/wallet"
 #define PKCERTCHAIN_BASE_SUBDIR   ".pkcertchain"
+#define PKCERTCHAIN_SIGN_PRIV_FILE "sign_priv.key"
+#define PKCERTCHAIN_SIGN_PUB_FILE  "sign_pub.key"
+#define PKCERTCHAIN_ENC_PRIV_FILE  "enc_priv.key"
+#define PKCERTCHAIN_ENC_PUB_FILE   "enc_pub.key"
 
 /*
  * Check whether ~/.pkcertchain/wallet exists.
@@ -92,6 +98,85 @@ UTIL_INLINE OpStatus_t ensure_wallet_dir(void)
 {
     if (!need_pkcertchain_setup()) return OP_SUCCESS;
     return create_wallet();
+}
+
+UTIL_INLINE OpStatus_t save_file_0600(const char *path, const uint8_t *buf, size_t len)
+{
+    if (!path || !buf || len == 0) return OP_INVALID_INPUT;
+
+    int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd < 0) {
+        if (errno == EACCES || errno == EPERM) return OP_NEEDS_PRIVILEGE;
+        return OP_INVALID_INPUT;
+    }
+
+    size_t written = 0;
+    while (written < len) {
+        ssize_t rc = write(fd, buf + written, len - written);
+        if (rc < 0) {
+            int err = errno;
+            close(fd);
+            if (err == EACCES || err == EPERM) return OP_NEEDS_PRIVILEGE;
+            return OP_INVALID_INPUT;
+        }
+        written += (size_t)rc;
+    }
+
+    if (close(fd) != 0) {
+        if (errno == EACCES || errno == EPERM) return OP_NEEDS_PRIVILEGE;
+        return OP_INVALID_INPUT;
+    }
+
+    return OP_SUCCESS;
+}
+
+UTIL_INLINE OpStatus_t save_keys_to_wallet(const char *priv_name,
+                                           const char *pub_name,
+                                           const uint256 *priv_key,
+                                           const uint256 *pub_key)
+{
+    if (!priv_key || !pub_key) return OP_NULL_PTR;
+
+    OpStatus_t st = ensure_wallet_dir();
+    if (st != OP_SUCCESS) return st;
+
+    const char *home = getenv("HOME");
+    if (!home || home[0] == '\0') return OP_INVALID_INPUT;
+
+    char priv_path[512];
+    char pub_path[512];
+    if (snprintf(priv_path, sizeof(priv_path), "%s/%s/%s", home, PKCERTCHAIN_WALLET_SUBDIR, priv_name) <= 0)
+        return OP_INVALID_INPUT;
+    if (snprintf(pub_path, sizeof(pub_path), "%s/%s/%s", home, PKCERTCHAIN_WALLET_SUBDIR, pub_name) <= 0)
+        return OP_INVALID_INPUT;
+
+    uint8_t priv_buf[UINT256_SIZE];
+    uint8_t pub_buf[UINT256_SIZE];
+    if (uint256_serialize_be(priv_key, priv_buf, sizeof(priv_buf)) != OP_SUCCESS) return OP_INVALID_INPUT;
+    if (uint256_serialize_be(pub_key, pub_buf, sizeof(pub_buf)) != OP_SUCCESS) return OP_INVALID_INPUT;
+
+    st = save_file_0600(priv_path, priv_buf, sizeof(priv_buf));
+    if (st != OP_SUCCESS) return st;
+    st = save_file_0600(pub_path, pub_buf, sizeof(pub_buf));
+    if (st != OP_SUCCESS) return st;
+
+    return OP_SUCCESS;
+}
+
+/*
+ * Save Ed25519 signing keys to ~/.pkcertchain/wallet with 0600 permissions.
+ */
+UTIL_INLINE OpStatus_t save_sign_keys(const uint256 *priv_key, const uint256 *pub_key)
+{
+    return save_keys_to_wallet(PKCERTCHAIN_SIGN_PRIV_FILE, PKCERTCHAIN_SIGN_PUB_FILE, priv_key, pub_key);
+}
+
+/*
+ * Save X25519 encryption keys to ~/.pkcertchain/wallet with 0600 permissions.
+ */
+UTIL_INLINE OpStatus_t save_enc_keys(const uint256 *priv_key, const uint256 *pub_key)
+{
+    return save_keys_to_wallet(PKCERTCHAIN_ENC_PRIV_FILE, PKCERTCHAIN_ENC_PUB_FILE, priv_key, pub_key);
 }
 
 #endif // LINUXUTILS_H
